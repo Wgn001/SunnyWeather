0e17133a93facbf56aae052ba2a1f3600c09aa04
Wgn001 <1281555629@qq.com>
2020-08-27 23:37:25 +0800

    添加天气信息

--- app/src/main/java/com/example/sunnyweather/android/logic/Repository.kt
+++ app/src/main/java/com/example/sunnyweather/android/logic/Repository.kt
@@ -2,10 +2,14 @@ package com.example.sunnyweather.android.logic
 
 import androidx.lifecycle.liveData
 import com.example.sunnyweather.android.logic.model.Place
+import com.example.sunnyweather.android.logic.model.Weather
 import com.example.sunnyweather.android.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.lang.Exception
 import java.lang.RuntimeException
+import kotlin.math.ln
 
 object Repository{
     fun searchPlaces(query:String) = liveData(Dispatchers.IO) {
@@ -25,4 +29,38 @@ object Repository{
         //发射结果
         emit(result)
     }
+
+
+    fun refreWeather(lng:String,lat:String)= liveData(Dispatchers.IO) {
+        val result=try {
+            coroutineScope {
+                val deferredRealtime=async {
+                    SunnyWeatherNetwork.getRealtimeWeather(lng,lat)
+                }
+
+                val deferredDaily=async {
+                    SunnyWeatherNetwork.getDailyWeather(lng,lat)
+                }
+
+                val realtimeResponse=deferredRealtime.await();
+                val dailyResponse=deferredDaily.await()
+
+                if (realtimeResponse.status=="ok" && dailyResponse.status=="ok"){
+                    val weather= Weather(realtimeResponse.result.realtime,
+                                            dailyResponse.result.daily)
+                    Result.success(weather)
+                }else{
+                    Result.failure(RuntimeException(
+                        "realtime response status is ${realtimeResponse.status}"+
+                                "daily response status is ${dailyResponse.status}"
+                    ))
+                }
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        emit(result)
+
+    }
+
 }
